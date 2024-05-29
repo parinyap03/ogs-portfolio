fc3b3d638310eeb4b6017e34c412ec838c4335c7
parinyap03 <parinyaporn.sa@kkumail.com>
2024-05-29 09:35:25 +0700

    [Add]:add hover card works

--- src/parallax/Work2.tsx
+++ src/parallax/Work2.tsx
@@ -116,7 +116,7 @@ const Work2 = () => {
               src={item.image}
               loading="lazy"
               alt={item.name}
-              className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              className="img-work absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
             />
 
             <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
